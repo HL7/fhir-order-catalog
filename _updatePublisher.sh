290fa385fa7328ddcd23bb14c7f6f5db89b09cb4
François Macary <francois.macary@phast.fr>
2020-07-31 11:01:32 +0200

    Updated all scripts for publisher.jar url + suppressed proprietaty codes from examples
    Tried to suppress the slicing info message in the ignoreWarnings.txt file but this does not work. So I suppressed the 3 Quest proprietary codes from the examples.
    I also suppressed the FHIR-order-catalog.xml file, which does not belong to this github repository but to the  http://github.com/hl7/jIRA-Spec-Artifacts project.

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -2,6 +2,6 @@
 mkdir -p input-cache
 cd input-cache
 echo "Downloading most recent publisher - it's ~100 MB, so this may take a bit"
-curl -o org.hl7.fhir.publisher.jar https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.publisher.jar
+curl -o org.hl7.fhir.publisher.jar https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar
 cd ..
 echo "Done"
